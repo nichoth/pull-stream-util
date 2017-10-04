f2804dce6c4740720ad380183814cb75d95d29de
Nick Thomas <nichoth@gmail.com>
2017-10-04 16:50:02 -0700

    http kluge

---
+++ example/http-stream.js
@@ -0,0 +1,52 @@
+var S = require('pull-stream')
+var createServer = require('http').createServer
+var HTTPStream = require('../http')
+var assert = require('assert')
+
+var Requests = HTTPStream({
+    headers: { foo: 'bar' },
+    body: { test: 'test' }
+})('http://localhost:8000')
+
+var server = createServer(function onRequest (req, res) {
+    assert.equal(req.headers.foo, 'bar')
+    var data = ''
+    req.on('data', function (d) {
+        data += d
+    })
+    req.on('end', function () {
+        assert.deepEqual(JSON.parse(data), {
+            test: 'test',
+            bar: 'baz'
+        })
+    })
+
+    res.end(JSON.stringify({ hello: 'world' }))
+})
+
+server.listen(8000, function () {
+    S(
+        Requests({ bar: 'baz' }),
+        S.collect(function (err, res) {
+            console.log('err', err)
+            var _res = res.map(function (ev) {
+                return {
+                    type: ev.type,
+                    req: ev.req,
+                    body: ev.body
+                }
+            })
+            console.log('res', _res)
+
+            assert.deepEqual(_res, [
+                { type: 'start', req: { bar: 'baz' }, body: undefined },
+                { type: 'resolve', req: { bar: 'baz' }, body: {
+                    hello: 'world' } }
+            ])
+            server.close()
+        })
+    )
+})
+
+
+
---
+++ http.js
@@ -0,0 +1,83 @@
+var xtend = require('xtend')
+var async = require('pull-async')
+var cat = require('pull-cat')
+var once = require('pull-stream/sources/once')
+var HTTPCall = require('./lib/http')
+var S = require('pull-stream/pull')
+var map = require('pull-stream/throughs/map')
+
+function httpStream (fn, onErr) {
+    return function (req) {
+        var result$ = async(function (cb) {
+            if (req) fn(req, onResponse)
+            else fn(onResponse)
+
+            function onResponse (err, resp, body) {
+                if (err) {
+                    if (onErr) return cb(null, onErr(err, req))
+                    return cb(err)
+                }
+
+                cb(null, {
+                    type: 'resolve',
+                    req: req,
+                    res: resp,
+                    body: body
+                })
+            }
+        })
+
+        return cat([
+            once({
+                type: 'start',
+                req: req
+            }),
+            result$
+        ])
+    }
+}
+
+function _onErr (err, req) {
+    return {
+        type: 'error',
+        req: req,
+        err: err
+    }
+}
+
+function fromObject (fns) {
+    var cid = 0
+
+    return Object.keys(fns).reduce(function (acc, k) {
+        return acc[k] = function (args) {
+            var _id = cid++
+            var reqStream = fns[k](args)
+
+            return S(
+                reqStream,
+                map(function (ev) {
+                    return xtend(ev, {
+                        cid: _id
+                    })
+                })
+            )
+        }
+    }, {})
+}
+
+function HTTPStream (opts, onErr) {
+    onErr = onErr === undefined ? _onErr : onErr
+    var call = HTTPCall(opts)
+    return function startStream (url, body) {
+        if (!body) return function (_body) {
+            return startStream(url, _body)
+        }
+        return httpStream(call(url), onErr)(body)
+    }
+}
+
+HTTPStream.fromAsyncFunction = httpStream
+HTTPStream.fromObject = fromObject
+
+module.exports = HTTPStream
+
